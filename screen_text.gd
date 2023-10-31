711b6c64181c6b1cc33360b82da78861cf235de0
MichaelFar <rarrafarrar@gmail.com>
2023-10-31 13:04:14 -0700

    Added total enemies defeated counter, added game complete screen

--- screen_text.gd
+++ screen_text.gd
@@ -3,17 +3,21 @@ extends Control
 @export var game_timer : Timer
 
 @export var text_node : RichTextLabel
-@export var initialMinutes = 5
+@export var initialMinutes = 3
+
+var totalEnemiesDefeated = 0
 var totalTimeSec = initialMinutes * 60
 var maxTime = totalTimeSec
 
 var minutes = totalTimeSec / 60
 var seconds = 0
 var playerDied = false
-
+var endGame = false
+signal game_complete
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#position = Vector2(0.0, get_viewport().size.y)
+	Globals.game_timer = self
 	text_node.text = update_text()
 	position.y = get_viewport_rect().size.y / -2.0
 	
@@ -22,8 +26,13 @@ func _process(delta):
 	if(totalTimeSec == maxTime):
 		Globals.player.has_died.connect(game_over)
 func _on_timer_timeout():
-	if(!playerDied):
+	if(!playerDied && !endGame):
 		totalTimeSec -= 1
+		if(totalTimeSec == 0):
+			game_complete.emit()
+			endGame = true
+			game_over()
+			return
 		if ((maxTime - totalTimeSec) % 10 == 0):
 			Globals.spawner.spawn_enemies()
 			Globals.spawner.spawnMin += 1
@@ -42,5 +51,7 @@ func update_text():
 	return actual_string
 
 func game_over():
-	playerDied = true
+	playerDied = !endGame
 	text_node.text = "GAME OVER \n Your final time was: " + update_text()
+	if(!playerDied):
+		text_node.text = "GAME COMPLETE!!! \n Total Enemies Defeated: " + str(totalEnemiesDefeated)
