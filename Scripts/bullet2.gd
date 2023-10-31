aba0548f738c8dac3d710800c726099206c9aa43
MichaelFar <rarrafarrar@gmail.com>
2023-10-31 12:44:08 -0700

    Added game over screen, scaling difficulty, timer, enemy death, trajectory randomization

--- Scripts/bullet2.gd
+++ Scripts/bullet2.gd
@@ -3,24 +3,20 @@ extends CharacterBody2D
 
 @export var damage = 10.0
 
-var yInfluence = 0.0
-func _ready():
-	var randObj = RandomNumberGenerator.new()
-	yInfluence = randObj.randf_range(-1000.0, 1000.0)
-
 
 func _physics_process(delta):
 	
 	velocity = transform.x * speed
-	velocity.y += yInfluence
-	velocity.x += yInfluence
 	move_and_slide()
 
-func _on_lifetime_timeout():
-	queue_free()
-
+func get_node_type():
+	return "projectile"
 
 func _on_hitbox_area_entered(area):
 	if(area.name == "Hurtbox" || area.name == "obstacle"):
+		print("Hit something as " + name)
 		queue_free()
 
+
+func _on_lifetime_timeout():
+	queue_free()
