9cc38c07bd5d3eef77d06126f4066f2f6a551fc8
MichaelFar <rarrafarrar@gmail.com>
2023-11-04 14:46:41 -0700

    Added knockback system, added hit shader to enemies

--- Scripts/chase_ai.gd
+++ Scripts/chase_ai.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 var playerNode = null
 
 @export var acceleration = 600.0
-@export var max_speed = 200.0
+@export var max_speed = 350.0
 @export var damage = 10
 @export var RayCastContainer : CharacterBody2D
 @export var raycast = true
@@ -11,7 +11,12 @@ var playerNode = null
 @export var health = 100.0
 @export var healthbar : ProgressBar
 @export var animationPlayer : AnimationPlayer
+@export var ShaderLoader : ResourcePreloader
 
+var shaderList = []
+
+var KBFrames = 0
+var KBApplied = false
 var frame = 0
 var shouldStop = false
 # Called when the node enters the scene tree for the first time.
@@ -20,7 +25,7 @@ func _ready():
 	healthbar.value = health
 	Globals.game_timer.game_complete.connect(freeze)
 	Globals.activeEnemies.append(self)
-	
+	shaderList = ShaderLoader.get_resource_list()
 	random_speed()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,16 +38,29 @@ func _physics_process(delta):
 		Globals.activeEnemies.pop_at(Globals.activeEnemies.find(self))
 		queue_free()
 	if(!shouldStop):
+		
 		var destination = playerNode.global_position - global_position 
 		
 		destination = destination.normalized()
+		
 		if(raycast):
 			RayCastContainer.supplied_direction = destination
 			
 			destination = RayCastContainer.suggestedVector
 		
 		sprite.look_at(playerNode.global_position)
-		velocity = velocity.move_toward(destination * max_speed, delta * acceleration)
+		
+		if(!KBApplied):
+	
+			velocity = velocity.move_toward(destination * max_speed, delta * acceleration)
+		else:
+			
+			KBFrames += 1
+			if(KBFrames / 5 == 1):
+				KBApplied = false
+				
+				material.set_shader_parameter("applied", false)
+				print("Shader is now unapplied")
 		move_and_slide()
 
 
@@ -53,7 +71,12 @@ func _on_hurtbox_area_entered(area):
 		print("Hit enemy")
 		health -= area.get_parent().damage
 		healthbar.value = health
-		
+		velocity = area.transform.x * (max_speed * area.get_parent().KBStrength / 3.0)
+		set_shader_time()
+		material.set_shader_parameter("applied", true)
+		KBFrames = 0
+		KBApplied = true
+		print("Shader is now applied")
 		
 func get_node_type():
 	return "enemy"
@@ -64,3 +87,7 @@ func random_speed():
 	
 func freeze():
 	shouldStop = true
+
+func set_shader_time():
+	
+	material.set_shader_parameter("start_time", Time.get_ticks_msec() / 1000.0)#Give time in seconds since engine has started
